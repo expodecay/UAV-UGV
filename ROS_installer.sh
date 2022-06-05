cdac19cfeb0377bec98bd85076624758cffe02da
Rick <expodecay@users.noreply.github.com>
2022-06-05 13:38:50 -0700

    Add files via upload
    Script installer for any Ubuntu version. At the prompt, provide the ROS codename for your Ubuntu version.
    Note: Ubuntu 20.04: ROS Noetic
              Ubuntu 18.04: ROS Melodic
              Ubuntu 16.04: ROS Kinetic

---
+++ ROS_installer.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Author : Rick Ramirez
+# California State Polytechnic University Pomona
+# Script follows here:
+
+echo "Which version of ROS do you need?"
+read ROS_VERSION
+echo "Installing ROS, $ROS_VERSION........."
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt install curl
+
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo apt update
+
+sudo apt install ros-$ROS_VERSION-desktop-full
+
+apt search ros-$ROS_VERSION
+
+echo "source /opt/ros/$ROS_VERSION/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+sudo apt install python-rosdep
+
+sudo rosdep init
+
+rosdep update
+
