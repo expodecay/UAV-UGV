4a084b14ea85783cd01ebd138a735487b7e3b71e
rick <rick.ramirez6174@gmail.com>
2022-06-05 20:20:33 -0700

    Adding catkin workspace generator

---
+++ workspace_generator.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Author : Rick Ramirez
+# California State Polytechnic University Pomona
+# Script follows here:
+
+echo "Please give your workspace a name.... Ex: catkin_ws"
+read WORKSPACE_NAME 
+echo "Generating, $WORKSPACE_NAME workspace........."
+
+mkdir -p ~/$WORKSPACE_NAME/src
+
+cd ~/$WORKSPACE_NAME/src
+
+catkin_init_workspace
+
+cd ..
+
+catkin_make
+
