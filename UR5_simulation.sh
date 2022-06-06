244308b1a95f1eb67d0afec5e93b108cc84d9f64
rick <rick.ramirez6174@gmail.com>
2022-06-05 23:36:39 -0700

    Adding install sctipt for UR5 arm

---
+++ UR5_simulation.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Author : Rick Ramirez
+# California State Polytechnic University Pomona
+# Script follows here:
+
+## Be sure to "source devel/setup.bash" the working directory if need be.
+echo "Please give your workspace a name.... Ex: catkin_ws"
+read WORKSPACE_NAME 
+echo "Generating, $WORKSPACE_NAME workspace........."
+
+mkdir -p ~/$WORKSPACE_NAME/src
+
+cd ~/$WORKSPACE_NAME/src
+
+catkin_init_workspace
+
+cd ..
+
+catkin_make
+
+echo "Which distribution of ROS are you developing on? melodic or noetic"
+read ROS_DISTRO
+echo "Generating, $ROS_DISTRO workspace........."
+
+git clone -b $ROS_DISTRO-devel https://github.com/ros-industrial/universal_robot.git
+
+cd $HOME/catkin_ws
+
+rosdep update
+
+rosdep install --rosdistro $ROS_DISTRO --ignore-src --from-paths src
+
+catkin_make
+
+source $HOME/catkin_ws/devel/setup.bash
+
+gnome-terminal -- "roslaunch ur_gazebo ur5.launch"
+sleep 1s
+
+nome-terminal -e "roslaunch ur5_moveit_config ur5_moveit_planning_execution.launch sim:=true"
+sleep 1s
+
+gnome-terminal -e "roslaunch ur5_moveit_config moveit_rviz.launch config:=true"
+
+# https://github.com/ros-industrial/universal_robot/tree/melodic-devel-staging
